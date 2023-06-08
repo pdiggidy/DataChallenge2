9e874f491a1c793e785a2ececfb0751ab4c088a9
Kajetan Knopp <kajetan@knopp.com.pl>
2023-06-08 16:22:58 +0200

    Added functionalities for modelling on predictions

--- Optimization/ILP_division.py
+++ Optimization/ILP_division.py
@@ -18,16 +18,24 @@ import pandas as pd
 # tested with Gurobi v9.0.0 and Python 3.7.0
 
 # Adding stuff
-data = pd.read_csv("../data/full_data.csv")
 wards = pd.read_csv("../DC2Data/Ward_assignments.csv")
 
 # Parameters
 ward_selection = set(list(wards["Ward"]))
-ward = "Finchley Church End Ward"
+
+result = []
+
+ward_data = pd.read_csv("../predictions.csv")
+ward_data_range = ward_data[["Ward", ward_data.columns[1]]].rename({ward_data.columns[1]: "Values"}, axis=1)
+print(ward_data_range)
+all_crimes = sum(ward_data_range["Values"].values.tolist())
 
 for ward in ward_selection:
-    officers = 5
-    budget = officers * 2
+    data = pd.read_csv("../data/full_data.csv")
+
+    crimes_ward = float(ward_data_range[ward_data["Ward"] == ward]["Values"])
+    officers = int(crimes_ward/all_crimes*200)
+    budget = officers
 
     LSOA_in_ward = wards.loc[wards.Ward == ward, :].dropna(axis=1).values.flatten().tolist()[1:]
 
@@ -45,7 +53,7 @@ for ward in ward_selection:
     # MIP  model formulation
     m = gp.Model("Police Distributions")
 
-    build = m.addVars(len(place), vtype=GRB.INTEGER, lb=0, ub=2, name="Hours here")
+    build = m.addVars(len(place), vtype=GRB.INTEGER, lb=0, ub=4, name="Hours here")
 
     m.addConstr(build.prod(cost) <= budget, name="budget")
 
@@ -53,6 +61,10 @@ for ward in ward_selection:
 
     m.optimize()
 
-    for el in build:
+    for i,el in enumerate(build):
+        result.append([LSOA_in_ward[i], build[el].x])
         if build[el].x > 0:
-            print("LSOA: ", el, "| Officers: ", build[el].x)
+            print("LSOA: ", LSOA_in_ward[i], "| Officers: ", build[el].x)
+
+result = pd.DataFrame(result, columns=["LSOA", "Officers"]).groupby("LSOA").sum()
+result.to_csv("../DC2Data/result_ILP.csv")
